cfce41e8bdaf3ca019a2fcd6db6a0baa33efdb8a
Sami6720 <78088136+Sami6720@users.noreply.github.com>
2023-08-12 11:36:31 -0400

    feat(dashboard): Remove Navbar from root layout.js
    - This means they would have to be added to every pagee where they are
    needed.

--- app/page.js
+++ app/page.js
@@ -2,10 +2,12 @@
 
 import { Container } from "@mui/material";
 import { HeroComp } from "./homeComponents/HeroComp";
+import NavbarComp from "./homeComponents/NavbarComp";
 
 export default function Home() {
   return (
     <main>
+      <NavbarComp />
       <Container>
         <HeroComp />
       </Container>
